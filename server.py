c52e6073f9c54159d331b80ffac947a1315337ff
Joel Seida <wip_race@yahoo.com>
2016-04-13 21:30:55 -0500

    Merged interrupt handling

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 def run_thread(connectionSocket, addr):
     try:
         message = connectionSocket.recv(4096)
-        print message
         filename = message.split()[1]
         f = open(filename[1:])
         outputdata = f.read()
@@ -12,13 +11,12 @@ def run_thread(connectionSocket, addr):
             connectionSocket.send(outputdata[i])
     except IOError:
         #Send response message for file not found
-        print "404"
         connectionSocket.send("HTTP/1.0 404 NOT FOUND\r\n\r\n404: File not found")
     except IndexError:
         pass
     #Close client socket
     connectionSocket.close()
-    
+
 #import socket module
 from socket import *
 import thread
@@ -26,13 +24,17 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a server socket
 serverSocket.bind(("localhost", 80))
 serverSocket.listen(5)
+serverSocket.settimeout(3)
+print 'Ready to serve...'
 try:
     while True:
-        #Establish the connection
-        print 'Ready to serve...'
-        (connectionSocket, addr) = serverSocket.accept()
-        #start thread
-        thread.start_new_thread(run_thread, (connectionSocket, addr))
+        try:
+            #Establish the connection
+            (connectionSocket, addr) = serverSocket.accept()
+            #start thread
+            thread.start_new_thread(run_thread, (connectionSocket, addr))
+        except timeout:
+            pass
 except KeyboardInterrupt:
     pass
 serverSocket.close()
