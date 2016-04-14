006982b0352f945d53f8f9142dfcb0acafea856a
Joel Seida <wip_race@yahoo.com>
2016-04-13 21:10:41 -0500

    Fixed timeout crash

--- server.py
+++ server.py
@@ -11,26 +11,27 @@ try:
         print 'Ready to serve...'
         try:
             (connectionSocket, addr) = serverSocket.accept()
-            message = connectionSocket.recv(4096)
-            print message
-            filename = message.split()[1]
-            f = open(filename[1:])
-            outputdata = f.read()
-            #Send one HTTP header line into socket
-            connectionSocket.send("HTTP/1.0 200 OK\r\n\r\n")
-            #Send the content of the requested file to the client
-            for i in range(0, len(outputdata)):
-                connectionSocket.send(outputdata[i])
-        except IOError:
-            #Send response message for file not found
-            print "404"
-            connectionSocket.send("HTTP/1.0 404 NOT FOUND\r\n\r\n404: File not found")
-        except IndexError:
-            pass
+            try:
+                message = connectionSocket.recv(4096)
+                print message
+                filename = message.split()[1]
+                f = open(filename[1:])
+                outputdata = f.read()
+                #Send one HTTP header line into socket
+                connectionSocket.send("HTTP/1.0 200 OK\r\n\r\n")
+                #Send the content of the requested file to the client
+                for i in range(0, len(outputdata)):
+                    connectionSocket.send(outputdata[i])
+            except IOError:
+                #Send response message for file not found
+                print "404"
+                connectionSocket.send("HTTP/1.0 404 NOT FOUND\r\n\r\n404: File not found")
+            except IndexError:
+                pass
+            #Close client socket
+            connectionSocket.close()
         except timeout:
             pass
-        #Close client socket
-        connectionSocket.close()
 except KeyboardInterrupt:
     pass
 serverSocket.close()
